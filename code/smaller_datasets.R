a9ce274fc733afe570d3f768c5547e9ee4902347
SebKrantz <sebastian.krantz@graduateinstitute.ch>
2026-03-26 14:24:56 -0400

    Remove unused functions.

--- code/smaller_datasets.R
+++ code/smaller_datasets.R
@@ -11,12 +11,6 @@ fetch_OCID <- function() {
   dest
 }
 
-load_OCID <- function() {
-  
-  fread("data/opencellid/cell_towers.csv.gz")
-  
-}
-
 load_GIP <- function() {
   
   readxl::read_xlsx("data/GEM/Global-Integrated-Power-March-2026.xlsx", sheet = 2) |> 
@@ -65,50 +59,10 @@ load_GEM_steel <- function() {
   
 }
 
-# Spatial Finance Initiative (Global Data on Certain Productive Assets)
-# https://www.cgfi.ac.uk/spatial-finance-initiative/geoasset-project/geoasset-databases/
-load_SFI <- function() {
-  
-  list(
-    cement = readxl::read_xlsx("data/SFI/SFI-Global-Cement-Database-July-2021.xlsx", 
-                               sheet = "SFI_ALD_Cement_Database"),
-    paper_pulp = readxl::read_xlsx("data/SFI/SFI_ALD_Pulp_Paper_Database_October_2024.xlsx", 
-                                   sheet = "SFI_Pulp_Paper_Database"),
-    steel = readxl::read_xlsx("data/SFI/SFI-Global-Steel-Database-July-2021.xlsx", 
-                              sheet = "SFI_ALD_Steel_Database"),
-    ethylene = readxl::read_xlsx("data/SFI/SFI_ALD_Global_Ethylene_Database_October_2024.xlsx", 
-                                 sheet = "Global_Ethylene_Production"),
-    beef = readxl::read_xlsx("data/SFI/SFI_ALD_Beef_Abattoirs_Top5_Dec_2022.xlsx", 
-                             sheet = "SFI_ALD_Beef_Abattoir_Top5")
-  ) |> 
-    lapply(function(x) get_vars(x, varying(x)) |> frename(tolower))
-  
-}
-
-
 # Open Zone Map: https://www.openzonemap.com/map
 load_OZM <- function() {
   
   fread("data/OZM/Open Zone Map raw data - The Adrianople Group - 2023.csv") |> 
-    janitor::clean_names() |> 
-    get_vars(varying)
-  
-}
-
-# World Port Index
-# https://msi.nga.mil/Publications/WPI
-fetch_WPI <- function() {
-
-  dir.create("data/WPI", recursive = TRUE, showWarnings = FALSE)
-  download.file("https://msi.nga.mil/api/publications/download?type=view&key=16920959/SFH00000/UpdatedPub150.csv",
-                destfile = "data/WPI/WPI.csv", method = "curl")
-
-  "data/WPI/WPI.csv"
-}
-
-load_WPI <- function() {
-  
-  fread("data/WPI/WPI.csv") |> 
     janitor::clean_names() |> 
     get_vars(varying)
   
@@ -215,14 +169,6 @@ load_solar_assets <- function() {
   
 }
 
-# https://zenodo.org/records/3628142#.YGOrmWhKhPY
-# https://github.com/carderne/predictive-mapping-global-power
-load_global_power_map <- function() {
-  
-  sf::st_read("data/EGM/grid.gpkg")
-  
-} 
-
 # Oil and Gas Infrastructure Mapping (OGIM); latest from Zenodo via fetch_OGIM()
 # https://doi.org/10.5281/zenodo.7466757
 load_OGIM <- function() {
@@ -261,67 +207,4 @@ load_ITU_nodes <- function() {
     
 }
 
-# Vector data ---------------------------------------------------
-
-# Global mining footprint mapped from high-resolution satellite imagery 
-# https://zenodo.org/records/7894216 (data/GMF)
-load_GMF <- function() {
-  
-  sf::st_read("data/GMF")
-  
-}
-
-# Global Roads Inventory Project
-# https://www.globio.info/download-grip-dataset
-# See also raster data available...
-load_GRIP <- function() {
-  
-  sf::st_read("data/GRIP/GRIP4_global_vector_fgdb/GRIP4_GlobalRoads.gdb")
-  
-}
-
-# OOKLA Internet Speeds
-# https://github.com/teamookla/ookla-open-data
-# https://www.speedtest.net/insights/blog/best-ookla-open-data-projects-2021/
-# R package: remotes::install_github("teamookla/ooklaOpenDataR")
-fetch_OOKLA <- function() {
-
-  year <- zoo::as.yearqtr(Sys.Date()) - 0.25
-  quarter <- as.integer(substr(year, 7, 7))
-  year <- as.integer(substr(year, 1, 4))
-
-  dir.create("data/OOKLA", recursive = TRUE, showWarnings = FALSE)
-  fixed <- ooklaOpenDataR::get_performance_tiles(service = "fixed", year = year, quarter = quarter)
-  fixed$tile <- NULL
-  qs::qsave(fixed, file = "data/OOKLA/OOKLA_fixed.qs")
-
-  mobile <- ooklaOpenDataR::get_performance_tiles(service = "mobile", year = year, quarter = quarter)
-  mobile$tile <- NULL
-  qs::qsave(mobile, file = "data/OOKLA/OOKLA_mobile.qs")
-
-  c("data/OOKLA/OOKLA_fixed.qs", "data/OOKLA/OOKLA_mobile.qs")
-}
-
-load_OOKLA <- function() {
-  
-  list(fixed = qs::qread("data/OOKLA/OOKLA_fixed.qs"), 
-       mobile = qs::qread("data/OOKLA/OOKLA_mobile.qs"))
-}
-
-
-
-# https://gee-community-catalog.org/projects/energy_farms/
-# -> Based on OSM data, not updated...
-load_global_solar <- function() {
-  
-  fread("data/global_wind_solar_2020/global_solar_2020_WGS84.csv") |> 
-    janitor::clean_names()
-  
-}
-load_global_wind <- function() {
-  
-  fread("data/global_wind_solar_2020/global_wind_2020_WGS84.csv") |> 
-    janitor::clean_names()
-  
-}
 
